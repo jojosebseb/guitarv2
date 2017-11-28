86470d5452f73ed9c47a71ae3035419309203a26
Jonathan Sebastian <jojosebseb@gmail.com>
2017-11-28 16:36:26 +0700

    v1

--- script/jquery.js
+++ script/jquery.js
@@ -1,8 +1,13 @@
-$('#menuHandle > .line-box').on('click', function(){
+function menuToggle(){
     $('#menuHandle').toggleClass('active');
     $('#globalFader').toggleClass('active');
+    $('#parentContainer').toggleClass('back-blur');
+    $('#navbar').toggleClass('back-blur');
+    $('#footer').toggleClass('back-blur');
+}
+$('#menuHandle > .line-box').on('click', function(){
+    menuToggle()
 });
 $('#globalFader').on('click', function(){
-    $('#menuHandle').toggleClass('active');
-    $('#globalFader').toggleClass('active');
+    menuToggle()
 });
